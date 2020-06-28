2db2f8a94baa0871c80a3c50e70d93a45428c385
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:18:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number) 
+  output = number / 3 == 0 ? "Fizz" : '' + number / 5 == 0 ? "Buzz" : '' 
+  if output = '' 
+    return nil 
+  end 
+  output
+end 
