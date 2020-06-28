15c56c9c0e08cb6315478735e7f515ccc2ad8c35
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:22:39 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(number) 
+  output = (number % 3 == 0 ? "Fizz" : '') + (number % 5 == 0 ? "Buzz" : '') 
+  if output.length > 0 
+    return output 
+  else 
+    return nil 
+  end
+end 
