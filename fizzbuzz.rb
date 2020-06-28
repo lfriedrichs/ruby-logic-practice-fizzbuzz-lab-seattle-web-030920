10cdf86fff2efffc61dda27b87fd65feee192c61
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:21:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,5 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number) 
-  (number / 3 == 0 ? "Fizz" : '') + (number / 5 == 0 ? "Buzz" : '') 
+  (number % 3 == 0 ? "Fizz" : '') + (number % 5 == 0 ? "Buzz" : '') 
 end 
